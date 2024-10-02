7a4ac96044787fe55534aaa6b4f96121d640d781
NickRsoo <nick.friesen@gmx.de>
2024-10-02 11:09:25 +0200

    database test

--- data.py
+++ data.py
@@ -1,1 +1,21 @@
+import sqlite3
+
+# Verbindung zur SQLite-Datenbank herstellen
+conn = sqlite3.connect('rpgpt.db')
+
+# Beispiel: Tabelle erstellen
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        race TEXT,
+        class TEXT,
+        level INTEGER DEFAULT 1,
+        xp INTEGER DEFAULT 0
+    )
+''')
+
+conn.commit()
+conn.close()
 
